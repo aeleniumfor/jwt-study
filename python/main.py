a262b40898068282ef2e28bfe1dae7b95654360a
fukuda <fukudammk@gmail.com>
2018-10-11 01:50:01 +0900

    add secret

--- python/main.py
+++ python/main.py
@@ -1,14 +1,28 @@
 import sys,re
-import base64,binascii
+import base64
 import json
+import hashlib,hmac
+import codecs
 
 args = sys.argv
 str = args[1].split('.')
-
-
-
-header = base64.b64decode(str[0]).decode("UTF-8")
-payload = base64.b64decode(str[1]).decode("UTF-8")
-
+print("----------------------------")
+print(args[1])
+header = base64.b64decode(str[0])
+payload = base64.b64decode(str[1])
+print("----------------------------")
 print(header)
 print(payload)
+print("----------------------------")
+
+signature = base64.b64encode(header)+b'.'+base64.b64encode(payload)
+print(signature)
+base64.urlsafe_b64encode
+
+secret = b'secret'
+
+signature = hmac.new(secret,signature, hashlib.sha256)
+print(str[2])
+signature = signature.hexdigest()
+print(signature.decode('hex'))
+##signature = [(i+j) for (i,j) in zip(signature[::2],signature[1::2])]
