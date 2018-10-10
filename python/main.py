eb41d36018d273ca539c6abd8c80ca8141fb3f4a
fukuda <fukudammk@gmail.com>
2018-10-11 02:26:10 +0900

    comp

--- python/main.py
+++ python/main.py
@@ -2,7 +2,7 @@ import sys,re
 import base64
 import json
 import hashlib,hmac
-import codecs
+import urllib.request
 
 args = sys.argv
 str = args[1].split('.')
@@ -15,14 +15,13 @@ print(header)
 print(payload)
 print("----------------------------")
 
-signature = base64.b64encode(header)+b'.'+base64.b64encode(payload)
+signature = base64.urlsafe_b64encode(header)+b'.'+base64.urlsafe_b64encode(payload)
 print(signature)
-base64.urlsafe_b64encode
-
 secret = b'secret'
 
 signature = hmac.new(secret,signature, hashlib.sha256)
 print(str[2])
-signature = signature.hexdigest()
-print(signature.decode('hex'))
-##signature = [(i+j) for (i,j) in zip(signature[::2],signature[1::2])]
+signature = signature.digest()
+signature = base64.urlsafe_b64encode(signature)
+# signature = [(i+j) for (i,j) in zip(signature[::2],signature[1::2])]
+print(signature)
