5e71b4f70e19cfd3a0ebac799ec37a89bc832421
fukuda <fukudammk@gmail.com>
2018-10-10 22:19:20 +0900

    header payload paese

--- python/main.py
+++ python/main.py
@@ -1,8 +1,14 @@
 import sys,re
-import base64
+import base64,binascii
+import json
 
 args = sys.argv
 str = args[1].split('.')
 
-print(base64.b64decode(str[0]))
-print(base64.b64decode(str[1]))
+
+
+header = base64.b64decode(str[0]).decode("UTF-8")
+payload = base64.b64decode(str[1]).decode("UTF-8")
+
+print(header)
+print(payload)
